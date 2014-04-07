28240c77c98859a47b5cd7c83b5ff22c06f689f1
if1live <libsora25@gmail.com>
2014-04-07 22:30:11 +0900

    add test src

---
+++ pelican_jsfiddle/test_jsfiddle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import docutils.core
+from pelican_jsfiddle import register as register_jsfiddle
+
+rest_text = '''
+
+.. jsfiddle:: if1live/V2P28
+
+.. jsfiddle:: if1live/V2P28
+   :width: 100%
+   :height: 150
+   :tabs: js,result
+   :skin: presentation
+
+'''
+
+register_jsfiddle()
+html = docutils.core.publish_string(source=rest_text)
+print html
